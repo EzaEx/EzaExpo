3290d6d1cdd736faff9154f9e9198f5467c0cb9c
Eza <tomlewisint@gmail.com>
2022-11-18 13:08:15 +0000

    Add cold planet

--- shader/show_shader.js
+++ shader/show_shader.js
@@ -50,17 +50,17 @@ function onLoaded(loader,res) {
     filter.uniforms.dims = [logicWidth, logicHeight];
 
 
-    filter.uniforms.altitude_col = [0, .6, 0];
+    filter.uniforms.altitude_col = [.1, .6, .1];
     filter.uniforms.terrain_col = [.45, 0, 0];
     filter.uniforms.arctic_col = [.6, .6, .6];
-    filter.uniforms.marine_col = [0., .1, .5];
+    filter.uniforms.marine_col = [.2, .2, .5];
     filter.uniforms.cloud_col = [.8, .8, .8];
 
     filter.uniforms.planet_scale = .7;
     filter.uniforms.altitude_scale = 4.;
     filter.uniforms.terrain_scale = 4.;
     filter.uniforms.sea_level = .5;
-    filter.uniforms.polar_boundary = 1.75;
+    filter.uniforms.polar_boundary = 1.55;
 
 
     if (urlParams.get('weird')) {
@@ -75,6 +75,21 @@ function onLoaded(loader,res) {
         filter.uniforms.terrain_scale = 15.;
         filter.uniforms.sea_level = .4;
         filter.uniforms.polar_boundary = 1.75;
+    }
+
+
+    if (urlParams.get('new')) {
+        filter.uniforms.altitude_col = [.3, .2, .1];
+        filter.uniforms.terrain_col = [.8, .8, .8];
+        filter.uniforms.arctic_col = [.9, .9, 1.];
+        filter.uniforms.marine_col = [.6, .6, .9];
+        filter.uniforms.cloud_col = [.8, .8, .8];
+
+        filter.uniforms.planet_scale = .5;
+        filter.uniforms.altitude_scale = 8.;
+        filter.uniforms.terrain_scale = 3.;
+        filter.uniforms.sea_level = .5;
+        filter.uniforms.polar_boundary = 1.;
     }
 
 
