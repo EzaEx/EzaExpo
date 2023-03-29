ec29f7809eaa1dfe9f64baa8159e0f8abf9ce1c4
Eza <tomlewisint@gmail.com>
2023-03-29 10:53:47 +0100

    add page

--- components.js
+++ components.js
@@ -8,12 +8,13 @@ class Header extends HTMLElement {
         let activeHref = this.getAttribute("active");
         
         this.innerHTML = /*html*/`
+            <h1>Ezaex</h1>
             <nav>
                 <hr>
                 <ul>
-                    <li><a href = "./"      class = ${activeHref == "./" ? "active" : ""}     >Home</a> </li>
-                    <li><a href = "./page2" class = ${activeHref == "./page2" ? "active" : ""}>Page2</a></li>
-                    <li><a href = "./page3" class = ${activeHref == "./page3" ? "active" : ""}>Page3</a></li>
+                    <li><a href = "/"      class = ${activeHref == "/" ? "active" : ""}     >Home</a> </li>
+                    <li><a href = "/about" class = ${activeHref == "/about" ? "active" : ""}>About</a></li>
+                    <li><a href = "/page3" class = ${activeHref == "/page3" ? "active" : ""}>Page3</a></li>
                 </ul>
                 <hr>
             </nav>
@@ -21,4 +22,18 @@ class Header extends HTMLElement {
     }
 }
 
+
+class Meta extends HTMLElement {
+    constructor() {
+        super();
+
+        this.innerHTML = /*html*/`
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href="/style.css" rel="stylesheet" type="text/css" />
+        `;        
+    }
+}
+
+customElements.define('c-meta', Meta);
 customElements.define('c-header', Header);
