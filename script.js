aa575e0007726a40524f7e582018c6da0ba6ebc2
EzaLaptop <tomlewisint@gmail.com>
2023-03-31 13:36:19 +0100

    add conway code

--- script.js
+++ script.js
@@ -6,22 +6,47 @@ ctx.fillStyle = "#ff0000";
 let w = canvas.width; let h = canvas.height;
 
 
+let array2 = []
+let array3 = initRandom2DArray(w, h)
+array2 = array3;
 
-let array = init2DArray(w, h)
-console.log(array)
-arrayCtx(array, ctx)
-
-
-
+function render() {
 
 
+    console.log(array3)
 
-function init2DArray(w, h) {
+    for (let y = 0; y < array3.length; y ++) {
+        for (let x = 0; x < array3[0].length; x ++) {
+            let n = 0;
+            for (let yd = -1; yd <= 1; yd ++) {
+                for (let xd = -1; xd <= 1; xd ++) {
+                    if(Math.abs(xd) + Math.abs(yd) != 1) {
+                        continue;
+                    }
+                    n++;
+                }
+            }
+            if (n == 0) array2[y][x] = 0;
+            else if (n < 3) array2[y][x] = 1;
+            else array2[y][x] = 0;
+        }
+    }
+    array3 = array2
+
+    arrayCtx(array3, ctx)
+    //setTimeout(render, 2000)
+}
+
+render()
+
+
+
+function initRandom2DArray(w, h) {
     let array = []
     for (let y = 0; y < h; y ++) {
         array.push([])
         for (let x = 0; x < w; x ++) {
-            array[y].push(1)
+            array[y].push(Math.round(Math.random()))
         }
     }
     return array;
@@ -29,6 +54,7 @@ function init2DArray(w, h) {
 
 
 function arrayCtx(array, ctx) {
+    ctx.clearRect(0, 0, w, h)
     for (let y = 0; y < array.length; y ++) {
         for (let x = 0; x < array[0].length; x ++) {
             if (array[y][x]) ctx.fillRect(x, y, 1, 1);
